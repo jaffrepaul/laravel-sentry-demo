49e63250d0f2ccaa5d915bc33043dfff3ca8f30b
paulj <jaffrepaul@gmail.com>
2025-03-23 13:53:57 -0400

    add form submission route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SubmissionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('form');
 });
+
+Route::post('/submit', [SubmissionController::class, 'store'])->name('submit.form');
