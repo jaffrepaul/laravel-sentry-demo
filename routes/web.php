1de10525ebf401f5dc2809a846dda8c30f57f1d1
paulj <jaffrepaul@gmail.com>
2025-03-23 23:30:46 -0400

    add error test route

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,8 @@ Route::get('/', function () {
     return view('form');
 });
 
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
+
 Route::post('/submit', [SubmissionController::class, 'store'])->name('submit.form');
